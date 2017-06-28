ba1895ac63f4900aa36cda457e5f2d7a1f23e09f
xulai1001 <xulai1001@gmail.com>
2017-06-28 17:35:43 +0800

    June 28

--- pagemap/v2ptran.c
+++ pagemap/v2ptran.c
@@ -12,92 +12,87 @@
 #define PFN_PRESENT_FLAG (1ul << 63)
 
 #define page_map_file  "/proc/self/pagemap"
+#define page_flag_file "/proc/kpageflags"
+#define mem_file "/dev/mem"
 #define PAGE_SIZE 0x1000ul      
 #define HUGE_SIZE 0x200000ul
-#define pad 0x3377BBFF      //random dumb
 
-#define ASSERT(line) if (!(line)) { fprintf(stderr, "ASSERT error at line %d: " #line "\n", __LINE__); return -1; }
+#define ASSERT(line) if (!(line)) { fprintf(stderr, "ASSERT error at line %d: " #line, __LINE__); perror(" "); return -1; }
 
-static int fd=-1; 
+static int fd=-1, flag_fd=-1, mem_fd=-1; 
 
-uint64_t v2p(uint64_t v) {
-    uint64_t vir_page_idx = v / PAGE_SIZE;      // 虚拟页号
-    uint64_t page_offset = v % PAGE_SIZE;       // 页内偏移
+uint64_t v2p(void *v) {
+    uint64_t vir_page_idx = (uint64_t)v / PAGE_SIZE;      // 虚拟页号
+    uint64_t page_offset = (uint64_t)v % PAGE_SIZE;       // 页内偏移
     uint64_t pfn_item_offset = vir_page_idx*sizeof(uint64_t);   // pagemap文件中对应虚拟页号的偏移
     
+    int dummy = *(int *)v;  // visit the addr, keep it in memory
     // 读取pfn
     uint64_t pfn_item, pfn;
     ASSERT( lseek(fd, pfn_item_offset, SEEK_SET) != -1 );
     ASSERT( read(fd, &pfn_item, sizeof(uint64_t)) == sizeof(uint64_t) );
-    ASSERT( pfn_item & PFN_PRESENT_FLAG );  // 确保页面存在
+   // ASSERT( pfn_item & PFN_PRESENT_FLAG );  // 确保页面存在
     pfn = pfn_item & PFN_MASK;              // 取低55位为物理页号
+    
+    uint64_t page_flags;
+    ASSERT( lseek(flag_fd, pfn*sizeof(uint64_t), SEEK_SET) != -1);
+    ASSERT( read(flag_fd, &page_flags, sizeof(uint64_t)) == sizeof(uint64_t) );
 
- //   printf("here vir=0x%lx, item=0x%lx, pfn=0x%lx, offset=%ld\n", v, pfn_item, pfn, page_offset);
+    printf("here vir=0x%lx, item=0x%lx, pfn=0x%lx, offset=%ld, flag=0x%lx\n", v, pfn_item, pfn, page_offset, page_flags);
 
     return pfn * PAGE_SIZE + page_offset;
 }
 
 void test_malloc(void)
 {
+    printf("test malloc...\n");
     int *a = malloc(sizeof(int) * HUGE_SIZE);
     int step = 0x20000, i;
     for (i=0; i<HUGE_SIZE; i+=step)
-        a[i]=pad;
-    for (i=0; i<HUGE_SIZE; i+=step)
         printf("a[%d], v = 0x%lx, p = 0x%lx\n", i, &a[i], v2p(&a[i]));
     free(a);
+}
+
+int test_mmap(void)
+{
+    printf("test_mmap...\n");
+    uint8_t *m;
+    uint64_t size = 10 * HUGE_SIZE, step=0x100000, i;
+    ASSERT((m = mmap(0, size, PROT_READ | PROT_WRITE,
+                         MAP_PRIVATE | MAP_ANONYMOUS | 0x40000, -1, 0)) != MAP_FAILED);
+    memset(m, 0, size);
+    for (i=0; i<size; i+=step)
+        printf("offset = 0x%lx, v = 0x%lx, p = 0x%lx\n", i, (uint64_t)m+i, v2p((uint64_t)m+i));    
+    munmap(m, size);
+    return 0;
+}
+
+int test_devmem(void)
+{
+    printf("test /dev/mem...\n");
+    char str[] = "A quick brown fox jumps over the lazy dog.";
+    char *page;
+    uint64_t p = v2p(str), page_start = p & ~0xffful;
+    
+    printf("v = 0x%lx, p = 0x%lx, page_start = 0x%lx\n", (uint64_t)str, p, page_start);
+
+    ASSERT( (page = mmap(0, PAGE_SIZE, PROT_READ,
+                             MAP_SHARED, mem_fd, page_start)) != MAP_FAILED);
+                             
+    printf("v = 0x%lx, p = 0x%lx, read = %s\n", (uint64_t)str, p, page[p & 0xfff]);
+    return 0;
 }
 
 int main(void)
 {
     printf("open %s...\n", page_map_file);
     ASSERT( (fd = open(page_map_file, O_RDONLY)) > 0 );
+    ASSERT( (flag_fd = open(page_flag_file, O_RDONLY)) > 0 );
+    ASSERT( (mem_fd = open(mem_file, O_RDONLY)) > 0 );
     test_malloc();
+    test_mmap();
+  //  test_devmem();    // can't do without rebuild kernel
+    
     return 0;
 }
-/*
-int main(int argc, char *argv[]) {
-  char *m;
-  size_t s = (10UL * 1024 * 1024);
-  m = mmap(NULL, s, PROT_READ | PROT_WRITE,
-                    MAP_PRIVATE | MAP_ANONYMOUS | 0x40000, -1, 0);
-  if (m == MAP_FAILED) {
-    perror("map mem");
-    m = NULL;
-    return 1;
-  }
-  memset(m, 0, s);
-  printf("map_hugetlb ok!\n");
-  unsigned long virAddress = (uint64_t)&m[0];
-  unsigned long phyAddress;
-  printf("VirAddress: 0x%lx\n",virAddress);
-  mem_addr_vir2phy(virAddress,&phyAddress);
-  printf("PhyAddress: 0x%lx\n",phyAddress);
 
-  virAddress = (uint64_t)&m[0x200000-2];
-  printf("VirAddress: 0x%lx\n",virAddress);
-  mem_addr_vir2phy(virAddress,&phyAddress);
-  printf("PhyAddress: 0x%lx\n",phyAddress);
-
-  virAddress = (uint64_t)&m[0x200000-1];
-  printf("VirAddress: 0x%lx\n",virAddress);
-  mem_addr_vir2phy(virAddress,&phyAddress);
-  printf("PhyAddress: 0x%lx\n",phyAddress);
-
-
-  virAddress = (uint64_t)&m[0x200000];
-  printf("VirAddress: 0x%lx\n",virAddress);
-  mem_addr_vir2phy(virAddress,&phyAddress);
-  printf("PhyAddress: 0x%lx\n",phyAddress);
-
-  virAddress = (uint64_t)&m[0x200000 + 1];
-  printf("VirAddress: 0x%lx\n",virAddress);
-  mem_addr_vir2phy(virAddress,&phyAddress);
-  printf("PhyAddress: 0x%lx\n",phyAddress);
-  getchar();
-  munmap(m, s);
-  return 0;
-}
-
-
-*/
