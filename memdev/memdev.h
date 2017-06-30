6f2cb56bbe9ec1e8a1e54482670aee022ccf5eca
viktor Xu <xulai1001@gmail.com>
2017-06-30 18:07:15 +0800

    add memdev module

---
+++ memdev/memdev.h
@@ -0,0 +1,32 @@
+#ifndef _MEMDEV_H
+#define _MEMDEV_H
+
+#include "linux/kernel.h"
+#include "linux/init.h"
+#include "linux/module.h"
+#include "linux/types.h"
+#include "linux/string.h"
+
+#include "linux/cdev.h"
+#include "linux/device.h"
+#include "linux/gfp.h"
+
+#define assert(expr) if (!(expr)) { printk("mem_dev:%d: ASSERT FAIL " # expr " failed!\n", \
+                        __LINE__); return 0;}
+// char device struct
+struct mem_dev
+{
+    struct file_operations *ops;
+    struct cdev cdev;
+};
+
+// these globals are implemented in memdev.c
+#ifndef MEMDEV_C
+extern int md_major, md_minor;
+extern struct class *md_class;
+extern struct mem_dev md_dev;
+extern struct class_device *md_device;
+extern struct file_operations md_fops;
+#endif
+
+#endif
